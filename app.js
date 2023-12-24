453000df5ea6c1b3d6b1a191cec8020cd97410aa
Aditya Singh <aditya9855@gmail.com>
2023-12-24 12:10:09 +0530

    colorupdate

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
-let turnO = true; //playerX, playerO
-let count = 0; //To Track Draw
+let turnO = true;
+let count = 0;
 
 const winPatterns = [
   [0, 1, 2],
@@ -30,10 +30,12 @@ boxes.forEach((box) => {
     if (turnO) {
       //playerO
       box.innerText = "O";
+      box.style.color= "green";
       turnO = false;
     } else {
       //playerX
       box.innerText = "X";
+      box.style.color= "black";
       turnO = true;
     }
     box.disabled = true;
